844f29f2f3a5ee089e3baaad3f6223a403715e51
David Chan <chand@chandeeland.org>
2013-02-05 22:00:21 -0500

    bug in default

--- ascii-art-generator.php
+++ ascii-art-generator.php
@@ -6,9 +6,10 @@ $ascii = new Ascii();
 
 if (isset($_GET['filename']) && !empty($_GET['filename'])) {
     $filename = 'images/' . $_GET['filename'] . '.png';
-    $image = imagecreatefrompng($filename);
 
-    echo $ascii->render($image);
 } else {   
-    echo $ascii->render('Lenna');
+    $filename = 'images/Lenna.png';
 }
+
+$image = imagecreatefrompng($filename);
+echo $ascii->render($image);
